b7e4a01866a730df2777c498bf6478eca750c71d
20221936clint <20221936@nbsc.edu.ph>
2025-05-28 02:52:12 +0800

    Updates

--- assets/ios.transition-legacy-izl0VdYS.js
+++ assets/ios.transition-legacy-izl0VdYS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dq2NCqDH.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BXnm4NbI.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
